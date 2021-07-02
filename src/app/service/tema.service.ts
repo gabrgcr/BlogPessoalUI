7f868884050e0c1db414918287858ddc8eb2b3a5
Gabriel <gabriel.cerdeira@outlook.com>
2021-07-02 10:29:11 -0300

    Feat: Criação e Consulta de temas

---
+++ src/app/service/tema.service.ts
@@ -0,0 +1,28 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { environment } from 'src/environments/environment.prod';
+import { tema } from '../model/tema';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TemaService {
+  constructor(private http: HttpClient) {}
+
+  token = {
+    headers: new HttpHeaders().set('Authorization', environment.token),
+  };
+
+  getAllTema(): Observable<tema[]> {
+    return this.http.get<tema[]>('http://localhost:8080/tema', this.token);
+  }
+
+  postTema(Tema: tema): Observable<tema> {
+    return this.http.post<tema>(
+      'http://localhost:8080/tema/',
+      Tema,
+      this.token
+    );
+  }
+}
--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,21 +1,42 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { environment } from 'src/environments/environment.prod';
+import { tema } from '../model/tema';
+import { TemaService } from '../service/tema.service';
 
 @Component({
   selector: 'app-tema',
   templateUrl: './tema.component.html',
-  styleUrls: ['./tema.component.css']
+  styleUrls: ['./tema.component.css'],
 })
 export class TemaComponent implements OnInit {
+  Tema: tema = new tema();
+  listaTemas: tema[];
 
-  constructor(private router:Router) { }
+  constructor(private router: Router, private temaService: TemaService) {}
 
   ngOnInit(): void {
     if (environment.token == '') {
       alert('Sua sessão expirou. Faça login novamente');
-      this.router.navigate(['/login'])
+      this.router.navigate(['/login']);
     }
+
+    this.findAllTemas();
   }
 
+  cadastrar() {
+    this.temaService.postTema(this.Tema).subscribe((resp: tema) => {
+      this.Tema = resp;
+      alert('Tema criado com sucesso!');
+      this.Tema = new tema();
+      this.findAllTemas();
+    });
+    this.findAllTemas();
+  }
+
+  findAllTemas() {
+    this.temaService.getAllTema().subscribe((resp: tema[]) => {
+      this.listaTemas = resp;
+    });
+  }
 }
